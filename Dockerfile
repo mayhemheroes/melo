909dd5cf363cb9a74a5c2ac2cbf2495099a5c9ca
ebell495 <ericbell495@gmail.com>
2022-09-30 14:12:43 -0700

    Use melo executable

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
 FROM ghcr.io/evanrichter/cargo-fuzz as builder
 
 ADD . /melo
-WORKDIR /melo/fuzz
-RUN cargo +nightly fuzz build 
+# WORKDIR /melo/fuzz
+# RUN cargo +nightly fuzz build 
+
+WORKDIR /melo
+RUN cargo build 
 
 FROM debian:bookworm
-COPY --from=builder /melo/fuzz/target/x86_64-unknown-linux-gnu/release/melo-fuzzer /
+# COPY --from=builder /melo/fuzz/target/x86_64-unknown-linux-gnu/release/melo-fuzzer /
+COPY --from=builder /melo/debug/melo /
 COPY --from=builder /melo/pieces /pieces/
--- fuzz/fuzz_targets/melo-fuzzer.rs
+++ fuzz/fuzz_targets/melo-fuzzer.rs
@@ -7,15 +7,9 @@ fn create_filename(instr: &str) -> Option<&str> {
     Some(instr)
 }
 
-fuzz_target!(|data: &[u8]| {
+fuzz_target!(|data: &str| {
     // fuzzed code goes here
-    let in_string = std::str::from_utf8(&data);
-    match in_string {
-        Ok(input) => {
-            let options = MidiGenerationOptions { ticks_per_beat: 16 };
-            let filename = "file";
-            let result = melo::compile_to_midi(&input, create_filename(filename), &options);
-        }
-        Err(_) => ()
-    }
+    let options = MidiGenerationOptions { ticks_per_beat: 16 };
+    let filename = "file";
+    let result = melo::compile_to_midi(&data, create_filename(filename), &options);
 });
